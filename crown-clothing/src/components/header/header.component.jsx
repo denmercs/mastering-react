8c47a59b8b270b2216eaa3821b93380a665b91d9
denmercs <denmercs@gmail.com>
2020-06-18 23:09:43 -0500

    working with authentication, google button works and implimented it on oauth

--- crown-clothing/src/components/header/header.component.jsx
+++ crown-clothing/src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase.util";
 
 import "./header.styles.scss";
 
@@ -17,6 +18,16 @@ const Header = ({ currentUser }) => (
       <Link className="option" to="/shop">
         CONTACT
       </Link>
+
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
   </div>
 );
