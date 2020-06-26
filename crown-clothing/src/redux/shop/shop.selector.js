20eb8772e2b6193c2124e231bc9e6acf026a28da
denmercs <denmercs@gmail.com>
2020-06-25 22:46:45 -0500

    added collection components and styles, still working on it

--- crown-clothing/src/redux/shop/shop.selector.js
+++ crown-clothing/src/redux/shop/shop.selector.js
@@ -1,11 +1,4 @@
 import { createSelector } from "reselect";
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
 
 const selectShop = (state) => state.shop;
 
@@ -15,9 +8,7 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) =>
-        collection.collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   );
